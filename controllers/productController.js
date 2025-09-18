8924565b7a55071db7b75678440a256faf973ef6
Larisa Hovhannisyan <larisa.hovhannisyan@benekiva.com>
2025-09-18 16:25:39 +0400

    edit/remove product

--- controllers/productController.js
+++ controllers/productController.js
@@ -24,3 +24,28 @@ exports.postAddProduct = async (req, res) => {
   await Product.create({ name, price, description });
   res.redirect("/dashboard");
 };
+
+exports.getEditProduct = async (req, res) => {
+  if (!req.session.user || req.session.user.role !== "admin") return res.send("Access denied.");
+
+  const product = await Product.findById(req.params.id);
+  if (!product) return res.send("Product not found");
+  res.render("editProduct", { user: req.session.user, product });
+};
+
+// Handle edit
+exports.postEditProduct = async (req, res) => {
+  if (!req.session.user || req.session.user.role !== "admin") return res.send("Access denied.");
+
+  const { name, price, description } = req.body;
+  await Product.findByIdAndUpdate(req.params.id, { name, price, description });
+  res.redirect("/dashboard");
+};
+
+// Handle delete
+exports.deleteProduct = async (req, res) => {
+  if (!req.session.user || req.session.user.role !== "admin") return res.send("Access denied.");
+
+  await Product.findByIdAndDelete(req.params.id);
+  res.redirect("/dashboard");
+};
--- routes/product.js
+++ routes/product.js
@@ -6,4 +6,11 @@ router.get("/dashboard", productController.listProducts);
 router.get("/products/add", productController.getAddProduct);
 router.post("/products/add", productController.postAddProduct);
 
+router.get("/products/edit/:id", productController.getEditProduct);
+router.post("/products/edit/:id", productController.postEditProduct);
+
+// Delete product
+router.get("/products/delete/:id", productController.deleteProduct);
+
+
 module.exports = router;
